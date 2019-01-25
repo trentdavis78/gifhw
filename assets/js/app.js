b92159efdba1a6ccaca2edb471ba41c06a60fc3f
Trent Davis <trentdavisinc@gmail.com>
2019-01-24 22:50:52 -0600

    Commit to send changes back to Desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
     var api = "EFpmfS0tkulMhfgMKidEgbG0MutwWY6A";
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+   
+      //jquery ajax method with query url and method (GET, UPDATE, DELETE) as parameters
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
 
-    //javascript, jQuery
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + api + "&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
+      //promise that returns the api response
+        .then(function(response) {
+
+        //dot notation of the object that returns the imageURL
+          var imageUrl = response.data.image_original_url;
+
+          //create a new IMG element
+          var catImage = $("<img>");
+
+          //attaches two attributes to the IMG tag
+          catImage.attr("src", imageUrl);
+          catImage.attr("alt", "cat image");
+
+          //prepends the img elem to the div with id images
+          $("#images").prepend(catImage);
+        });
 });
