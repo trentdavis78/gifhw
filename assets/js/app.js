ee5a86850e0b6581cebc3adfffb2d3cbef251212
Trent Davis <trendavisinc@gmail.com>
2019-01-26 08:45:07 -0600

    Added hover effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,58 @@
-$(document).ready(function(){
-    var api = "EFpmfS0tkulMhfgMKidEgbG0MutwWY6A";
-    // Create a request variable and assign a new XMLHttpRequest object to it.
-   
-      //jquery ajax method with query url and method (GET, UPDATE, DELETE) as parameters
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
+$(document).ready(function () {
+  var api = "EFpmfS0tkulMhfgMKidEgbG0MutwWY6A";
+  var query = "netflix";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    query + "&limit=10&api_key=" + api;
 
-      //promise that returns the api response
-        .then(function(response) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function (response) {
+      console.log(queryURL);
 
-        //dot notation of the object that returns the imageURL
-          var imageUrl = response.data.image_original_url;
+      console.log(response);
+      // storing the data from the AJAX request in the results variable
+      var results = response.data;
 
-          //create a new IMG element
-          var catImage = $("<img>");
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
 
-          //attaches two attributes to the IMG tag
-          catImage.attr("src", imageUrl);
-          catImage.attr("alt", "cat image");
+        // Creating and storing a div tag
+        var newLi = $("<li>");
+        newLi.addClass("d-inline pr-1");
+        // Creating a paragraph tag with the result item's rating
+        // var p = $("<p>").text("Rating: " + results[i].rating);
 
-          //prepends the img elem to the div with id images
-          $("#images").prepend(catImage);
-        });
+        // Creating and storing an image tag
+        var newImg = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+
+        newImg.attr("data-still", results[i].images.fixed_height_still.url);
+        newImg.attr("data-animated", results[i].images.fixed_height.url);
+        var stillSrc = newImg.attr("data-still");
+        newImg.attr("src", stillSrc);
+        newImg.addClass("newImgs");
+
+
+        // Appending the paragraph and image tag to the animalDiv
+        // newDiv.append(p);
+        newLi.append(newImg);
+
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gifRow1").prepend(newLi);
+      }
+    });
+  $(document.body).on('mouseenter', '.newImgs', function () {
+    $(this).addClass('transition');
+    newSrc = $(this).attr("data-animated");
+    $(this).attr("src", newSrc)
+  });
+  $(document.body).on('mouseleave', '.newImgs', function () {
+    $(this).removeClass('transition');
+    newSrc = $(this).attr("data-still");
+    $(this).attr("src", newSrc)
+  });
+
 });
