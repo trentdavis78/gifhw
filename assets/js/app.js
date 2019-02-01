e0071e14bf7d3c951df84e4be5df670c6fb3cf08
Trent Davis <trendavisinc@gmail.com>
2019-01-31 22:26:01 -0600

    Final commit.. I swear

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
       cleanString = cleanString.toLowerCase();
       newLi.text(cleanString);
       newLi.attr("data-array-index", i);
-      divId = camelize(cleanString);      
+      divId = camelize(cleanString);
       newLi.attr("data-div-id", divId)
       $("#exploreUl").append(newLi);
     }
@@ -24,23 +24,23 @@ $(document).ready(function () {
       method: "GET"
     })
       // After data comes back from the request
-      .then(function (response) {        
+      .then(function (response) {
         var results = response.data;
         query = camelize(query);
         query = query.toLowerCase();
         cleanString = removeSpecialChars(query);
-        
+
         // write title to HTML
-        var titleRow =  '<div id="' + cleanString + '" class="row px4pc pt-5">';
-            titleRow += '<div class="col-12 row-title">';
-            titleRow += '<h4>' + cleanString + '</h4>';
-            titleRow +=  '</div></div>';
+        var titleRow = '<div id="' + cleanString + '" class="row px4pc pt-5">';
+        titleRow += '<div class="col-12 row-title">';
+        titleRow += '<h4>' + cleanString + '</h4>';
+        titleRow += '</div></div>';
         var sliderSection = $("<section>");
-            sliderSection.addClass("center slider");
+        sliderSection.addClass("center slider");
         var fullscreenSection = $("<section class='fullscreen' id='" + cleanString + "FS'>");
         var fssHtml = "<i data-section-id='" + cleanString + "' class='fas fa-times'></i>";
-            fssHtml += "<div id='fssMainContent'></div>";
-            fullscreenSection.html(fssHtml);
+        fssHtml += "<div id='fssMainContent'></div>";
+        fullscreenSection.html(fssHtml);
 
         // Looping through each result item and spitting out HTML
         for (var i = 0; i < results.length; i++) {
@@ -55,20 +55,20 @@ $(document).ready(function () {
           newDiv.append(newImg);
           var overlayDiv = $("<div>");
           overlayDiv.addClass("overlayDiv");
-          var width =  results[i].images.fixed_height.width;
-          overlayDiv.css({"width": width});
+          var width = results[i].images.fixed_height.width;
+          overlayDiv.css({ "width": width });
           var title = cleanTitle(results[i].title);
-          var html = "<div class='overlayContent'><h5>" + title + "</h5>";   
-          html += "<span class='maturity'>" +  results[i].rating  + "</span>";
+          var html = "<div class='overlayContent'><h5>" + title + "</h5>";
+          html += "<span class='maturity'>" + results[i].rating + "</span>";
           html += "<svg class='down-arrow' data-gif-id='" + results[i].id + "' data-section-id='" + cleanString + "' width='45px' height='15px'><polyline fill='none' stroke='#FFFFFF' stroke-width='1' stroke-miterlimit='10' points='0.846,1.404 21.783,13.537 42.833,1.404 '/></svg>";
           html += "</div>";
-          overlayDiv.html(html);          
+          overlayDiv.html(html);
           newDiv.append(overlayDiv);
           $(sliderSection).prepend(newDiv);
-          
+
         }
         // attach slick slider to the row
-        $(sliderSection).slick({          
+        $(sliderSection).slick({
           infinite: true,
           speed: 300,
           slidesToShow: 1,
@@ -79,67 +79,67 @@ $(document).ready(function () {
         $("#gifWrapper").prepend(sliderSection);
         $("#gifWrapper").prepend(titleRow);
       });
-      
-  }  
+
+  }
   // camelize search strings 
   function camelize(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
       if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
       return index == 0 ? match.toLowerCase() : match.toUpperCase();
     });
   }
   // remove special chars from strings
   function removeSpecialChars(str) {
-    str = str.replace(/[^a-zA-Z 0-9]+/g,"");
+    str = str.replace(/[^a-zA-Z 0-9]+/g, "");
     return str;
-    }
+  }
   // remove "GIF" and "By.." from Title
   function cleanTitle(str) {
-    if(str.indexOf("by ") > 0){
+    if (str.indexOf("by ") > 0) {
       str = str.substr(0, str.lastIndexOf("by "));
-    }          
+    }
     str = str.replace(/gif/gi, " ");
     return str;
   }
   // mouse hover effects 
-  $(document.body).on('mouseenter', '.overlayDiv', function () {       
+  $(document.body).on('mouseenter', '.overlayDiv', function () {
     $(this).addClass('transition fadeIn');
     $(this).siblings().addClass('transition');
     newSrc = $(this).siblings().attr("data-animated");
-    $(this).siblings().attr("src", newSrc);    
-    
-  });  
-  $(document.body).on('mouseleave', '.overlayDiv', function () {       
+    $(this).siblings().attr("src", newSrc);
+
+  });
+  $(document.body).on('mouseleave', '.overlayDiv', function () {
     $(this).removeClass('transition fadeIn');
     $(this).siblings().removeClass('transition');
     newSrc = $(this).siblings().attr("data-still");
-    $(this).siblings().attr("src", newSrc);        
-     
+    $(this).siblings().attr("src", newSrc);
+
   });
-  $(document.body).on('click', '.down-arrow', function () {    
+  $(document.body).on('click', '.down-arrow', function () {
     $(".fullscreen").fadeOut();
-    $(".overlayDiv").addClass("transitionDisable");   
-    $(".slick-slide").addClass("transitionDisable");  
-    var gifID =  $(this).attr("data-gif-id");      
-    var sectionID = $(this).attr("data-section-id");    
+    $(".overlayDiv").addClass("transitionDisable");
+    $(".slick-slide").addClass("transitionDisable");
+    var gifID = $(this).attr("data-gif-id");
+    var sectionID = $(this).attr("data-section-id");
     fullscreenQuery(gifID, sectionID);
     $("#" + sectionID + "FS").fadeIn();
-    var targetOffset = $("#" + sectionID ).offset().top + 200;
-      $('html, body').animate({
-        scrollTop: targetOffset
+    var targetOffset = $("#" + sectionID).offset().top + 200;
+    $('html, body').animate({
+      scrollTop: targetOffset
     }, 500);
   });
-  $(document.body).on('click', '.fa-times', function () {      
-    $(".overlayDiv").removeClass("transitionDisable");   
-    $(".slick-slide").removeClass("transitionDisable");      
+  $(document.body).on('click', '.fa-times', function () {
+    $(".overlayDiv").removeClass("transitionDisable");
+    $(".slick-slide").removeClass("transitionDisable");
     var sectionID = $(this).attr("data-section-id");
     $("#" + sectionID + "FS").fadeOut();
   });
-  $(document.body).on('click', '#exploreUl li', function () {      
+  $(document.body).on('click', '#exploreUl li', function () {
     var divId = $(this).attr("data-div-id");
     var targetOffset = $("#" + divId).offset().top - 60;
-      $('html, body').animate({
-        scrollTop: targetOffset
+    $('html, body').animate({
+      scrollTop: targetOffset
     }, 500);
   });
   // search button 
@@ -157,45 +157,45 @@ $(document).ready(function () {
       e.preventDefault();
       $("#searchButton").click();
     }
-  
+
   });
   // fullscreen api call 
   function fullscreenQuery(gifID, sectionID) {
     var queryURL = "https://api.giphy.com/v1/gifs/" +
-    gifID + "?api_key=" + api;
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    // After data comes back from the request
-    .then(function (response) { 
-      var result = response.data;
-      var title = cleanTitle(result.title);
-      var size =parseInt(result.images.original.size)/1000000;
-      size = Math.round(size * 100) / 100;
-      var options = { year: 'numeric', month: 'long', day: 'numeric' };
-      var date = new Date(result.import_datetime);
-      date = date.toLocaleDateString("en-US", options);
-      console.log(result);
-      var fssMCHtml = "<div id='fssMCDesc'><h1>" + title +"</h1>";
-          fssMCHtml += "<h2>Rated <span class='maturity fsMaturity'>" + result.rating + "</span>"
-          fssMCHtml += "<h4>Source: ";
-          fssMCHtml += "<a href='" + result.source_post_url + "' target='_blank'>" + result.source_tld +"</a></h4>";
-          fssMCHtml += "<h6>Uploaded: " + date  + "</h6>";
-          fssMCHtml += "<h6>Dimensions: " + result.images.original.width + " x " +  result.images.original.height + " px</h6>";
-          fssMCHtml += "<h6>Size: " + size + " MB</h6>";          
-          fssMCHtml += "</div>";
-          fssMCHtml += "<div id='fssMCImg'><div id='ImgGradient'></div><img src='" + result.images.original.url+ "'></div>";
-      $("#" + sectionID + "FS").find("#fssMainContent").html(fssMCHtml);
-    });
-    
+      gifID + "?api_key=" + api;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function (response) {
+        var result = response.data;
+        var title = cleanTitle(result.title);
+        var size = parseInt(result.images.original.size) / 1000000;
+        size = Math.round(size * 100) / 100;
+        var options = { year: 'numeric', month: 'long', day: 'numeric' };
+        var date = new Date(result.import_datetime);
+        date = date.toLocaleDateString("en-US", options);
+        console.log(result);
+        var fssMCHtml = "<div id='fssMCDesc'><h1>" + title + "</h1>";
+        fssMCHtml += "<h2>Rated <span class='maturity fsMaturity'>" + result.rating + "</span>"
+        fssMCHtml += "<h4>Source: ";
+        fssMCHtml += "<a href='" + result.source_post_url + "' target='_blank'>" + result.source_tld + "</a></h4>";
+        fssMCHtml += "<h6>Uploaded: " + date + "</h6>";
+        fssMCHtml += "<h6>Dimensions: " + result.images.original.width + " x " + result.images.original.height + " px</h6>";
+        fssMCHtml += "<h6>Size: " + size + " MB</h6>";
+        fssMCHtml += "</div>";
+        fssMCHtml += "<div id='fssMCImg'><div id='ImgGradient'></div><img src='" + result.images.original.url + "'></div>";
+        $("#" + sectionID + "FS").find("#fssMainContent").html(fssMCHtml);
+      });
+
   }
   // initialize 
   function initCategories() {
-    for(i=0;i<categoryArray.length;i++){
+    for (i = 0; i < categoryArray.length; i++) {
       queryAPI(categoryArray[i]);
     }
   }
   initCategories();
-  printCategories(categoryArray);    
+  printCategories(categoryArray);
 });
